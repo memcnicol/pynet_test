92a3c5329d280d1f72b41cd12287e120cdb278ee
mcnicol.mac@gmail.com <mcnicol.mac@gmail.com>
2017-06-20 12:59:13 -0700

    added split functionality

--- ips.py
+++ ips.py
@@ -5,13 +5,15 @@ ip3 = "192,168.1.1"
 print("{:^90}".format("having fun!"))
 print("{:^30}{:^30}{:^30}".format("test 1", "test 2", "test3"))
 print("{:^30}{:^30}{:^30}".format(ip1, ip2, ip3))
-ip4 = input("Please input another IP address: ")
-ip4 = ip4.split(".")
-if(len(ip4) != 4):
-    print("Error")
-else:
-    print("{:^30}{:^30}{:^30}{:^30}".format("octet 1", "octet 2", "octet 3", "octet 4"))
-    print("{:^30}{:^30}{:^30}".format(*ip4))
 
-
+while(True):
+    ip4 = input("Please input another IP address: ")
+    ip4 = ip4.split(".")
+    if(len(ip4) != 4):
+        print("IP format invalid. ")
+    elif(len(ip4) == 4):
+        check = True
+        print("{:^30}{:^30}{:^30}{:^30}".format("octet 1", "octet 2", "octet 3", "octet 4"))
+        print("{:^30}{:^30}{:^30}{:^30}".format(*ip4))
+        break
 
