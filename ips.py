f470e24f03f00cc696147ba48950e82eeac1c155
mcnicol.mac@gmail.com <mcnicol.mac@gmail.com>
2017-06-20 12:45:27 -0700

    added IPs example

---
+++ ips.py
@@ -0,0 +1,17 @@
+ip1 = "10.0.0.1"
+ip2 = "69.69.0.1"
+ip3 = "192,168.1.1"
+
+print("{:^90}".format("having fun!"))
+print("{:^30}{:^30}{:^30}".format("test 1", "test 2", "test3"))
+print("{:^30}{:^30}{:^30}".format(ip1, ip2, ip3))
+ip4 = input("Please input another IP address: ")
+ip4 = ip4.split(".")
+if(len(ip4) != 4):
+    print("Error")
+else:
+    print("{:^30}{:^30}{:^30}{:^30}".format("octet 1", "octet 2", "octet 3", "octet 4"))
+    print("{:^30}{:^30}{:^30}".format(*ip4))
+
+
+
